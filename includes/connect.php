86fe302e7d5d05588ad7355853df4e4829678cb2
Brian Turza <turza.brian@gmail.com>
2022-06-17 00:40:11 +0200

    deploy fix

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?PHP
 // connect to the database
-
+/*
 $db_host = $_ENV['host'];
 $db_user = $_ENV['username'];
 $db_pass = $_ENV['password'];
@@ -10,5 +10,5 @@ $db =  mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 if (!$db) {
     die("Connection failed: " . mysqli_error());
 } 
-
+*/
 ?>
